230566e1f102637b8cae2d624749ecc490a055d8
Robert Morigi-Eades <robe16@users.noreply.github.com>
2017-03-05 00:33:34 +0000

    Create login_creds.py

---
+++ src/primaryessence/login_creds.py
@@ -0,0 +1,7 @@
+# EXAMPLE FILE - TO BE UPDATED WITH PERSONAL DETAILS
+
+PREFIX = 'beb'
+USERNAME = 'beb.forename.surname'
+PASSWORD = 'password'
+NURSERY = 'bridgeend-brighouse'
+CHILD_ID = '123456'
