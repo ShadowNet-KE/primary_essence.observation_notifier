068b13fd9fb3c94b823637ce39a23348f91442cf
Rob <robe16@hotmail.co.uk>
2017-04-05 00:02:54 +0100

    Update to only run the code between 'hours of opening'. This will reduce number of calls made during the night when updates are unlikely to be uploaded. Hours of opening hardcoded within run.py, but easily changed if required by user.

--- src/run.py
+++ src/run.py
@@ -1,5 +1,5 @@
 from time import sleep
-from datetime import datetime
+import datetime
 from primaryessence.get_data import get_learningJournal
 from primaryessence.session import create_session
 from primaryessence.parse_observations import find_observations
@@ -19,9 +19,23 @@ while True:
         #
         send_notifications_all(child_id, o[child_id])
     #
+    now = datetime.datetime.now()
+    nxt = now + datetime.timedelta(hours=1)
+    #
+    s = 7    # opening time of nursery in 24 hours
+    e = 18   # closing time of nursery in 24 hours
+    #
+    if nxt.time() >= datetime.time(e):
+        nxt = datetime.datetime(now.year, now.month, now.day+1, datetime.time(s).hour, 0, 0, 0)
+    elif nxt.time() < datetime.time(s):
+        nxt = datetime.datetime(now.year, now.month, now.day, datetime.time(s).hour, 0, 0, 0)
+    #
     print('****************************************************************')
-    print('Operation run at {time}'.format(time=datetime.now().strftime('%Y-%m-%d %H:%M')))
+    print('Operation run at:            {dt}'.format(dt=now.strftime('%Y-%m-%d %H:%M')))
+    print('Next scheduled run will be:  {dt}'.format(dt=nxt.strftime('%Y-%m-%d %H:%M')))
     print('****************************************************************')
     #
-    sleep(3600) # hourly
+    slp = (nxt - now).seconds
+    #
+    sleep(slp)
     #
