d1490d56e4220432b68a1a81f0b0ab4d3af5ecea
Rob <robe16@hotmail.co.uk>
2017-06-18 23:07:05 +0100

    Page parsing updated to accomoodate for new html following update by primary essence in early June 2017..

--- src/primaryessence/parse_observations.py
+++ src/primaryessence/parse_observations.py
@@ -10,80 +10,89 @@ def find_observations(s, data, child_id):
     #
     o = []
     #
-    divs = soup.findAll("div", {"class": "LJItemPrintWrapper"})
-    for div in divs:
-        id = div.parent.attrMap['id'].replace('ljPrintItemWrapper_', '')
+    div_items = soup.findAll("div", {"class": "item"})
+    for item in div_items:
         #
-        if not check_history(child_id, id):
-            # title
-            title = div.contents[1].contents[0]
-            title = trimStrings(title)
+        for div in item.contents:
             #
-            img = []
-            vid = []
-            #
-            imgs = div.findAll("img")
-            if len(imgs) > 0:
-                for i in imgs:
+            try:
+                #
+                if div.attrMap['id'].startswith('ljPrintItemWrapper_'):
                     #
-                    src = False
-                    onclick = False
+                    id = div.attrMap['id'].replace('ljPrintItemWrapper_', '')
                     #
-                    for img_attr in i.attrs:
-                        if img_attr[0] == 'src':
-                            src = img_attr[1]
-                    #
-                    if bool(src):
-                        r = s.get(src)
-                        if r.ok:
-                            img.append(r.content)
-            else:
-                v_imgs = div.parent.parent.findAll("img", {"src": "/Content/Images/Master/video-placeholder.png"})
-                for v_img in v_imgs:
-                    #
-                    onclick = False
-                    #
-                    for img_attr in v_img.attrs:
-                        if img_attr[0] == 'onclick':
-                            onclick = img_attr[1]
-                    #
-                    if bool(onclick):
-                        # id = onclick.replace("GetPresignedVideoToPlay('/Observations/GetVideoForBrowserPlay/", "")
-                        # id = id.replace("/{child_id}');".format(child_id=child_id), "")
+                    if not check_history(child_id, id):
+                        # title
+                        title = div.contents[1].contents[1].contents[0]
+                        title = trimStrings(title)
                         #
-                        if not check_history(child_id, id):
-                            #
-                            url = onclick.replace("GetPresignedVideoToPlay('", "")
-                            url = url.replace("');", "")
-                            url = 'https://www.primaryessence.co.uk' + url
-                            #
-                            r_vid = s.post(url)
-                            #
-                            url_vid = r_vid.content
-                            url_vid = json.loads(url_vid)['preSignedUrl']
-                            #
-                            r = s.get(url_vid)
-                            if r.ok:
-                                vid.append(r.content)
-            #
-            #
-            div_othernotes = div.findAll(["h4", "h5", "p"])
-            comment = div.findAll("div", {"class": "LjItemPrintableCommentWrapper"})[0]
-            div_othernotes.insert(1, comment)
-            #
-            notes = ''
-            #
-            for item in div_othernotes:
-                if item.name == 'div':
-                    name = 'p'
-                else:
-                    name = item.name
-                notes += '<{tag}>{content}</{tag}>'.format(tag=name,
-                                                           content=item.text)
+                        img = []
+                        vid = []
+                        #
+                        imgs = div.findAll("img")
+                        if len(imgs) > 0:
+                            for i in imgs:
+                                #
+                                src = False
+                                onclick = False
+                                #
+                                for img_attr in i.attrs:
+                                    if img_attr[0] == 'src':
+                                        src = img_attr[1]
+                                #
+                                if bool(src):
+                                    r = s.get(src)
+                                    if r.ok:
+                                        img.append(r.content)
+                        else:
+                            v_imgs = div.parent.findAll("img", {"src": "/Content/Images/Master/video-placeholder.png"})
+                            for v_img in v_imgs:
+                                #
+                                onclick = False
+                                #
+                                for img_attr in v_img.attrs:
+                                    if img_attr[0] == 'onclick':
+                                        onclick = img_attr[1]
+                                #
+                                if bool(onclick):
+                                    # id = onclick.replace("GetPresignedVideoToPlay('/Observations/GetVideoForBrowserPlay/", "")
+                                    # id = id.replace("/{child_id}');".format(child_id=child_id), "")
+                                    #
+                                    if not check_history(child_id, id):
+                                        #
+                                        url = onclick.replace("GetPresignedVideoToPlay('", "")
+                                        url = url.replace("');", "")
+                                        url = 'https://www.primaryessence.co.uk' + url
+                                        #
+                                        r_vid = s.post(url)
+                                        #
+                                        url_vid = r_vid.content
+                                        url_vid = json.loads(url_vid)['preSignedUrl']
+                                        #
+                                        r = s.get(url_vid)
+                                        if r.ok:
+                                            vid.append(r.content)
+                        #
+                        #
+                        div_othernotes = div.findAll(["h4", "h5", "p"])
+                        comment = div.findAll("div", {"class": "LjItemPrintableCommentWrapper"})[0]
+                        div_othernotes.insert(1, comment)
+                        #
+                        notes = ''
+                        #
+                        for item in div_othernotes:
+                            if item.name == 'div':
+                                name = 'p'
+                            else:
+                                name = item.name
+                            notes += '<{tag}>{content}</{tag}>'.format(tag=name,
+                                                                       content=item.text)
 
-            #
-            # Create observation object and add to list
-            o.append(ObjObservation(id, title, notes, imgs=img, vids=vid))
+                        #
+                        # Create observation object and add to list
+                        o.append(ObjObservation(id, title, notes, imgs=img, vids=vid))
+            except Exception as e:
+                pass
     #
     return o
 
